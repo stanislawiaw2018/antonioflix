d1c8faec13d709c153ece055016f316d8426ba60
Antonio Stanislaw <stanislaw.iaw2018@gmail.com>
2020-08-03 01:20:40 -0300

    Implemented the single page application

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/img/Logo.png';
 import { LogoImage, MenuWrapper} from './style.js';
 import Button from '../Button';
@@ -7,12 +8,12 @@ import Button from '../Button';
 function Menu(){
     return(
         <MenuWrapper className="Menu">
-            <a href="/">
+            <Link to="/">
                 <LogoImage src={Logo} alt="Logo AntonioFlix" />
 
-            </a>
+            </Link>
 
-            <Button as="a" href="/">
+            <Button as={Link} className="ButtonLink" to="/cadastro/video">
                 Novo vídeo
             </Button>
         </MenuWrapper>
