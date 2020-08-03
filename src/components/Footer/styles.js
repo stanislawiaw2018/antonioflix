40a9e8ba1cb021158fbf0fa1a8a9a7043ccb634c
Antonio Stanislaw <stanislaw.iaw2018@gmail.com>
2020-08-03 02:40:02 -0300

    Created the registration routes category and videos

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -4,10 +4,7 @@ import styled from 'styled-components';
 export const FooterBase = styled.footer`
   background: var(--black);
   border-top: 2px solid var(--danger);
-  padding-left: 16px;
-  padding-right: 16px;
-  padding-top: 32px;
-  padding-bottom: 32px;
+  padding: 32px 16px;
   color: var(--white);
   text-align: center;
   @media (max-width: 800px) {
--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Home from './pages/Home';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import CadastroVideo from './pages/cadastro/video';
+import CadastroVideo from './pages/Cadastro/video';
+import CadastroCategoria from './pages/Cadastro/Categoria';
 
 
 
@@ -14,6 +15,7 @@ ReactDOM.render(
     <Switch>
       <Route path="/" component= {Home} exact />
       <Route path="/cadastro/video" component= {CadastroVideo} />
+      <Route path="/cadastro/categoria" component= {CadastroCategoria} />
       <Route component= {Pagina404}  />
     </Switch>
   </BrowserRouter>,
